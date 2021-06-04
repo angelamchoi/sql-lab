2890747e305594e9e69eb6256a61398557ff97c1
Angela Choi <angelamchoi@gmail.com>
2021-06-04 10:11:03 -0400

    finished sql lab

--- clues.sql
+++ clues.sql
@@ -32,14 +32,16 @@ SELECT * FROM city WHERE countrycode='SMR';
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
--- Write SQL query here
+SELECT * FROM city WHERE name LIKE 'Serra%';
+SELECT * FROM country WHERE code='BRA';
+
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
--- Write SQL query here
+SELECT * FROM city WHERE id='211';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -57,4 +59,7 @@ SELECT * FROM city WHERE countrycode='SMR';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+SELECT * FROM city WHERE population=91084;
+
+-->Carmen Sandiego is in Santa Monica, California!!!!
 
